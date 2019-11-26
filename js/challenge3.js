6aa79ff7c07ad0c2fceca1737b59911d83e20b3c
jieliang98 <54827972+jieliang98@users.noreply.github.com>
2019-11-25 21:17:39 -0500

    Update challenge3.js

--- js/challenge3.js
+++ js/challenge3.js
@@ -1,7 +1,7 @@
 $(document).ready(function(event){ 
     $('#ch3form').submit(function(event) {
-        if(!$('input[name="fruit"]:checked').length) {
-        	alert("You must pick a fruit!");
+        if(!$('input[name="fruit"]:checked').length || !$('input[name="standing"]:checked').length) {
+        	alert("You must pick a fruit and standing!");
         	event.preventDefault();
         } 
     });
